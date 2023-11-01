74b97ecffd282a5dd11f701775d82b2f10a0f617
Kemal <outsider.pld@gmail.com>
2023-11-01 23:11:04 +0000

    Tutorial - Rest Api, JWT, MongoDB

---
+++ go-mongodb/controllers/auth.controller.go
@@ -0,0 +1,46 @@
+package controllers
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/KemalBekir/Go-Tutorials/go-mongodb/models"
+	"github.com/KemalBekir/Go-Tutorials/go-mongodb/services"
+	"github.com/gin-gonic/gin"
+)
+
+type AuthController struct {
+	authService services.AuthService
+	userService services.UserService
+}
+
+func NewAuthController(authService services.AuthService, userService services.UserService) AuthController {
+	return AuthController{authService, userService}
+}
+
+func (ac *AuthController) SignUpUser(ctx *gin.Context) {
+	var user *models.SignUpInput
+
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": err.Error()})
+		return
+	}
+
+	if user.Password != user.PasswordConfirm {
+		ctx.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": "Password do not match"})
+		return
+	}
+
+	newUser, err := ac.authService.SignUpUser(user)
+
+	if err != nil {
+		if strings.Contains(err.Error(), "email already exists") {
+			ctx.JSON(http.StatusConflict, gin.H{"status": "error", "message": err.Error()})
+			return
+		}
+		ctx.JSON(http.StatusBadGateway, gin.H{"status": "error", "message": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{"status": "success", "data": gin.H{"user": models.FIlteredResponse(newUser)}})
+}
