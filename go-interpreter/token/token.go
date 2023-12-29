2a486d454764dce1c1a2c45f28af3d315ab4ec3b
Kemal <outsider.pld@gmail.com>
2023-12-29 18:37:44 +0000

    Second commit - fixed issue with tests

--- go-interpreter/token/token.go
+++ go-interpreter/token/token.go
@@ -11,13 +11,23 @@ const (
 	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"
 
+	EQ     = "=="
+	NOT_EQ = "!="
+
 	// Identifiers + literals
 	IDENT = "IDENT" // add, foobar, x, y, ...
 	INT   = "INT"   // 1343456
 
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
 
 	// Delimeters
 	COMMA     = ","
@@ -31,4 +41,26 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
