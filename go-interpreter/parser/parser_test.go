4813eec09171ba776547bc6efce889b6ef0a33d2
Kemal <outsider.pld@gmail.com>
2024-01-15 18:17:58 +0000

    Added - parsing index operator expressions - evaluating array literals -evaluating index operator expressions

--- go-interpreter/parser/parser_test.go
+++ go-interpreter/parser/parser_test.go
@@ -387,6 +387,14 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
 			"add(a + b + c * d / f + g)",
 			"add((((a + b) + ((c * d) / f)) + g))",
 		},
+		{
+			"a * [1, 2, 3, 4][b * c] * d",
+			"((a * ([1, 2, 3, 4][(b * c)])) * d)",
+		},
+		{
+			"add(a * b[2], b[1], 2 * [1, 2][1])",
+			"add((a * (b[2])), (b[1]), (2 * ([1, 2][1])))",
+		},
 	}
 
 	for _, tt := range tests {
@@ -752,57 +760,25 @@ func TestParsingArrayLiterals(t *testing.T) {
 	testInfixExpression(t, array.Elements[2], 3, "+", 3)
 }
 
-// TODO: uncomment when needed
-// func TestCallExpressionParameterParsing(t *testing.T) {
-// 	tests := []struct {
-// 		input         string
-// 		expectedIdent string
-// 		expectedArgs  []string
-// 	}{
-// 		{
-// 			input:         "add();",
-// 			expectedIdent: "add",
-// 			expectedArgs:  []string{},
-// 		},
-// 		{
-// 			input:         "add(1);",
-// 			expectedIdent: "add",
-// 			expectedArgs:  []string{"1"},
-// 		},
-// 		{
-// 			input:         "add(1, 2 * 3, 4 + 5);",
-// 			expectedIdent: "add",
-// 			expectedArgs:  []string{"1", "(2 * 3)", "(4 + 5)"},
-// 		},
-// 	}
+func TestParsingIndexExpressions(t *testing.T) {
+	input := "myArray[1 + 1]"
 
-// 	for _, tt := range tests {
-// 		l := lexer.New(tt.input)
-// 		p := New(l)
-// 		program := p.ParseProgram()
-// 		checkParserErrors(t, p)
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
 
-// 		stmt := program.Statements[0].(*ast.ExpressionStatement)
-// 		exp, ok := stmt.Expression.(*ast.CallExpression)
-// 		if !ok {
-// 			t.Fatalf("stmt.Expression is not ast.CallExpression. got=%T",
-// 				stmt.Expression)
-// 		}
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	indexExp, ok := stmt.Expression.(*ast.IndexExpression)
+	if !ok {
+		t.Fatalf("exp not *ast.IndexExpression. got=%T", stmt.Expression)
+	}
 
-// 		if !testIdentifier(t, exp.Function, tt.expectedIdent) {
-// 			return
-// 		}
+	if !testIdentifier(t, indexExp.Left, "myArray") {
+		return
+	}
 
-// 		if len(exp.Arguments) != len(tt.expectedArgs) {
-// 			t.Fatalf("wrong number of arguments. want=%d, got=%d",
-// 				len(tt.expectedArgs), len(exp.Arguments))
-// 		}
-
-// 		for i, arg := range tt.expectedArgs {
-// 			if exp.Arguments[i].String() != arg {
-// 				t.Errorf("argument %d wrong. want=%q, got=%q", i,
-// 					arg, exp.Arguments[i].String())
-// 			}
-// 		}
-// 	}
-// }
+	if !testInfixExpression(t, indexExp.Index, 1, "+", 1) {
+		return
+	}
+}
