946f8bfb4f66b7667667aaaad0a814fbc54d0994
Kemal <outsider.pld@gmail.com>
2024-03-14 19:20:46 +0000

    Exercise - hackerrank

---
+++ hackerrank/easy/cut-the-sticks.go
@@ -0,0 +1,23 @@
+package main
+
+import "sort"
+
+func cutTheSticks(arr []int32) []int32 {
+	res := make(map[int]int32)
+	result := make([]int32, 0, len(arr))
+	l := int32(len(arr))
+	for _, i := range arr {
+		res[int(i)]++
+	}
+	keys := make([]int, 0, len(res))
+	for key := range res {
+		keys = append(keys, key)
+	}
+	sort.Ints(keys)
+	for _, i := range keys {
+		result = append(result, l)
+		l = l - res[i]
+	}
+	return result
+
+}
