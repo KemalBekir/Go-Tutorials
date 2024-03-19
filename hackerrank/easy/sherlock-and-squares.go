46c0ef496c6ebdfa215d67e490f861485036cb13
Kemal Bekir <kbekir.work@gmail.com>
2024-03-19 23:24:56 +0000

    Exercise - hackerrank

---
+++ hackerrank/easy/sherlock-and-squares.go
@@ -0,0 +1,10 @@
+package main
+
+func squares(a int32, b int32) int32 {
+	left, right := int32(math.Sqrt(float64(a))), int32(math.Sqrt(float64(b)))
+		if left*left < a {
+			left++
+		}
+		return right - left + 1
+	
+	}
