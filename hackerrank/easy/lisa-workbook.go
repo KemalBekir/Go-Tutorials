b22b52c553399243e2e0cfafb309fddc5b4501f5
KemalBekir <outsider_pld@gmail.com>
2024-04-07 18:23:34 +0100

    Exercise - hackerrank

---
+++ hackerrank/easy/lisa-workbook.go
@@ -0,0 +1,23 @@
+package main
+
+func workbook(n int32, k int32, arr []int32) int32 {
+	pageNumber := int32(0)
+	amountOfSpecialProblems := int32(0)
+
+	for i := int32(0); i < n; i++ {
+		problemsInChapter := arr[i]
+		pageNumber++
+
+		for j := int32(1); j <= problemsInChapter; j++ {
+			if j == pageNumber {
+				amountOfSpecialProblems++
+			}
+
+			if j%k == 0 && j != problemsInChapter {
+				pageNumber++
+			}
+		}
+	}
+
+	return amountOfSpecialProblems
+}
